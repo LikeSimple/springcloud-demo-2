aaa7ac72d34c7aebdb9028578d85d11cd70dfcba
Jimmy.Xu <wolfxz@foxmail.com>
2019-10-20 20:18:50 +0800

    update spring cloud to SR3 update spring boot to 2.1.9 fix issues

--- config-server/src/main/java/com/newtouch/cloud/demo/config/ConfigApplication.java
+++ config-server/src/main/java/com/newtouch/cloud/demo/config/ConfigApplication.java
@@ -2,14 +2,12 @@ package com.newtouch.cloud.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableConfigServer
-@EnableRedisHttpSession
 public class ConfigApplication {
 
     public static void main(String[] args) {
--- service/user/src/main/resources/db/migration/V1.1__ALTER_USERNAME_UNIQUE.sql
+++
@@ -1,3 +0,0 @@
-drop index idx_system_user_username on system_user;
-create unique index idx_system_user_username
-    on system_user (username);
