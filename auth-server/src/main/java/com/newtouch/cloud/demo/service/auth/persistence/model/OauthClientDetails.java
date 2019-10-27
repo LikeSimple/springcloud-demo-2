4afab881356e6e648b92a9af429d410714224514
Jimmy.Xu <wolfxz@foxmail.com>
2019-10-27 21:31:36 +0800

    add oauth client model and mapper

---
+++ auth-server/src/main/java/com/newtouch/cloud/demo/service/auth/persistence/model/OauthClientDetails.java
@@ -0,0 +1,32 @@
+package com.newtouch.cloud.demo.service.auth.persistence.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class OauthClientDetails implements Serializable {
+    private String clientId;
+
+    private String resourceIds;
+
+    private String clientSecret;
+
+    private String scope;
+
+    private String authorizedGrantTypes;
+
+    private String webServerRedirectUri;
+
+    private String authorities;
+
+    private Integer accessTokenValidity;
+
+    private Integer refreshTokenValidity;
+
+    private String additionalInformation;
+
+    private String autoapprove;
+
+    private static final long serialVersionUID = 1L;
+}
