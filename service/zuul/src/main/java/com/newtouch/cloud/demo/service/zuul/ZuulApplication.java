750aa49475a37b44ecf7149260e2bb9c6519f4bb
Jimmy Xu <zhen.xu@newtouch.com>
2019-11-28 14:19:58 +0800

    finish config support

--- service/zuul/src/main/java/com/newtouch/cloud/demo/service/zuul/ZuulApplication.java
+++ service/zuul/src/main/java/com/newtouch/cloud/demo/service/zuul/ZuulApplication.java
@@ -1,20 +1,13 @@
 package com.newtouch.cloud.demo.service.zuul;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.context.request.RequestContextListener;
 
 @SpringCloudApplication
 @EnableEurekaClient
@@ -29,14 +22,14 @@ public class ZuulApplication {
         SpringApplication.run(ZuulApplication.class, args);
     }
 
-    @Bean
-    public RequestContextListener requestContextListener() {
-        return new RequestContextListener();
-    }
+//    @Bean
+//    public RequestContextListener requestContextListener() {
+//        return new RequestContextListener();
+//    }
 
-    @Bean
-    @LoadBalanced
-    OAuth2RestTemplate oauth2RestTemplate(@Qualifier("oauth2ClientContext") OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
-        return new OAuth2RestTemplate(details, oauth2ClientContext);
-    }
+//    @Bean
+//    @LoadBalanced
+//    OAuth2RestTemplate oauth2RestTemplate(@Qualifier("oauth2ClientContext") OAuth2ClientContext oauth2ClientContext, OAuth2ProtectedResourceDetails details) {
+//        return new OAuth2RestTemplate(details, oauth2ClientContext);
+//    }
 }
